6c8b9342c76f820c69c58a1fb015c6f3bd260bfd
Beksultan Tatenov <bekatatenov@gmail.com>
2021-04-26 16:18:59 +0600

    initial commit

---
+++ src/main/java/com/peklo/peklo/PekloApplication.java
@@ -0,0 +1,13 @@
+package com.peklo.peklo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PekloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PekloApplication.class, args);
+    }
+
+}
