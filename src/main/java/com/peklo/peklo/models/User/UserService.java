5cf0ff5575d06f287379ae0d5b5380386aa16e40
Beksultan Tatenov <bekatatenov@gmail.com>
2021-04-28 17:44:31 +0600

    #3 add Service(UserService) with method findbyEmail which takes from repository

---
+++ src/main/java/com/peklo/peklo/models/User/UserService.java
@@ -0,0 +1,14 @@
+package com.peklo.peklo.models.User;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
