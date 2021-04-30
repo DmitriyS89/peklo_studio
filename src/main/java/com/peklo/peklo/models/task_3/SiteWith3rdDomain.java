bed9e987926b46394fc330b0251a9023d420da16
tariel sapar <tariel.fifa2015@gmail.com>
2021-04-30 18:30:54 +0600

    #6
    Added new class with interface and methods

---
+++ src/main/java/com/peklo/peklo/models/task_3/SiteWith3rdDomain.java
@@ -0,0 +1,13 @@
+package com.peklo.peklo.models.task_3;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SiteWith3rdDomain implements SiteWithDomain {
+    private String protocol;
+    private String domain_3rd;
+    private String domain_2rd;
+    private String domain_1rd;
+}
---
+++ src/main/java/com/peklo/peklo/models/task_3/SiteWithDomain.java
@@ -0,0 +1,8 @@
+package com.peklo.peklo.models.task_3;
+
+public interface SiteWithDomain {
+    String getProtocol();
+    String getDomain_3rd();
+    String getDomain_2rd();
+    String getDomain_1rd();
+}
