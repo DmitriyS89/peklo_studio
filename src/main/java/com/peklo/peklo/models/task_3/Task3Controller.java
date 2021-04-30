6bbe2c974487d7a65099968c50b38bc4bedf3e20
tariel sapar <tariel.fifa2015@gmail.com>
2021-04-30 18:33:19 +0600

    #6
    Changed Task3Service to interface
    Added class Task3ServiceImpl
    Changed logic tool_3
    Added new method which redirect to result

--- src/main/java/com/peklo/peklo/models/task_3/Task3Controller.java
+++ src/main/java/com/peklo/peklo/models/task_3/Task3Controller.java
@@ -5,25 +5,30 @@ import lombok.RequiredArgsConstructor;
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping(path = "tool_3")
 public class Task3Controller {
-    private final Task3Service task3Service;
+    private final Task3ServiceImpl task3ServiceImpl;
 
     @PostMapping("run")
     public String runTask3(@RequestParam String url, Model model) throws UrlNotConnection {
-        Document baseUrl = task3Service.getJSoupConnection(url);
-        List<Task3Element> results = task3Service.run(baseUrl);
-        model.addAttribute("results", results);
+        Document document = task3ServiceImpl.getJSoupConnection(url);
+        SiteWithDomain siteWith3rdDomain = task3ServiceImpl.makeSiteEntity(url);
+        List<Task3Element> results = task3ServiceImpl.filterPatterns(task3ServiceImpl.getLinks(document), siteWith3rdDomain);
+        model.addAttribute("results", task3ServiceImpl.filter((results)));
         model.addAttribute("error", "ok");
+        model.addAttribute("fromUrl", url);
         return "tool_3";
+    }
+
+    @GetMapping("run")
+    public String redirectToTool_3(){
+        return "redirect:http://localhost:8080/tool_3/result";
     }
 
     @GetMapping("result")
--- src/main/java/com/peklo/peklo/models/task_3/Task3Service.java
+++ src/main/java/com/peklo/peklo/models/task_3/Task3Service.java
@@ -1,136 +1,31 @@
 package com.peklo.peklo.models.task_3;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
 import com.peklo.peklo.exceptions.UrlNotConnection;
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.*;
-import java.util.regex.Matcher;
+import java.util.List;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
-@Service
-public class Task3Service {
-    private static final String STR_NULL = "NULL";
-    public static final Task3Element NULL_ELEMENT = new Task3Element(STR_NULL, STR_NULL);
-    private static String BASE_URL;
-    private static final String urlForExcel = "C:\\Users\\User\\GitClones\\peklo_studio\\results.xlsx";
+public interface Task3Service {
 
-    public List<Task3Element> run(Document document){
-        List<Task3Element> lists = new ArrayList<>();
-        for (Element element : document.getElementsByTag("a")){
-            lists.add(getValues(element));
-        }
-        return filter(lists);
-    }
+    List<Task3Element> getLinks(Document document);
 
-    private List<Task3Element> filter(List<Task3Element> lists) {
-        List<Task3Element> results = new ArrayList<>();
-        for ( Task3Element element : lists ) {
-            Task3Element result = filterFieldWithPattern("#", element.getHref(), element);
-            result = filterFieldWithPattern(BASE_URL, result.getHref(), result);
-            result = filterFieldWithPattern("/", result.getHref(), result);
-            results.add(result);
-        }
-        results = filterTargetValues(results);
-        results = filterGeneralNull(results);
-        return results;
-    }
+    Task3Element getLink(Element element);
 
-    public List<Task3Element> filterGeneralNull(List<Task3Element> elements) {
-        return elements.stream()
-                .filter(value -> !NULL_ELEMENT.equals(value))
-                .collect(Collectors.toList());
-    }
+    String getAttr(Element element, String attr);
 
-    public Task3Element getValues(Element element){
-        String target = getAttr(element, "target");
-        String href = getAttr(element, "href");
-        return new Task3Element(target, href);
-    }
+    List<Task3Element> filter(List<Task3Element> elements);
 
-    public String getAttr(Element element, String attr) {
-        String result = element.attributes().get(attr);
-        return "".equals(result) ? STR_NULL : result;
-    }
+    List<Task3Element> filterPatterns(List<Task3Element> elements, SiteWithDomain site);
 
-    public List<Task3Element> filterTargetValues(List<Task3Element> elements){
-        return elements.stream()
-                .filter(value -> !"_blank"  .equals(value.getTarget()))
-                .filter(value -> STR_NULL  .equals(value.getTarget()))
-                .collect(Collectors.toList());
-    }
+    Document getJSoupConnection(String url) throws UrlNotConnection;
 
-    public Task3Element filterFieldWithPattern(String word, String value, Task3Element element) {
-        Pattern pattern = Pattern.compile(String.format("^%s", word));
-        Matcher matcher = pattern.matcher(value);
-        if(matcher.find())
-            return NULL_ELEMENT;
-        else
-            return element;
-    }
+    Pattern getCompile(String url, String protocol, String domain3, String domain2, String domain1);
+    Pattern getCompile(String protocol, String domain2, String domain1);
+    Pattern getCompile(String protocol, String domain2);
 
-    public Document getJSoupConnection(String url) throws UrlNotConnection {
-        try {
-            Connection connect = Jsoup.connect(url);
-            BASE_URL = connect.get().baseUri();
-            return connect.get();
-        } catch (IOException e) {
-            throw new UrlNotConnection();
-        }
-    }
+    SiteWithDomain makeSiteEntity(String url);
 
-    public void makeExcel(List<Task3Element> elements, String url) {
-        Map<Integer, String[]> forExcel = new HashMap<>();
-        forExcel.put(0, new String[]{"URL->", url});
-        int count = 1;
-        for (Task3Element element : elements) {
-            String[] value = new String[]{"href->", element.getHref()};
-            forExcel.put(count, value);
-            count++;
-        }
-
-        XSSFWorkbook workbook = new XSSFWorkbook();
-        XSSFSheet spreadsheet = workbook.createSheet("Data by target attribute");
-        XSSFRow row;
-
-        Set<Integer> keyIds = forExcel.keySet().stream()
-                .sorted(Integer::compareTo)
-                .collect(Collectors.toSet());
-
-        int rowId = 0;
-
-        for (int key : keyIds) {
-            row = spreadsheet.createRow(rowId);
-            rowId++;
-
-            Object[] objectArr = forExcel.get(key);
-            int cellId = 0;
-            for (Object obj : objectArr) {
-                Cell cell = row.createCell(cellId);
-                cell.setCellValue((String)obj);
-                cellId++;
-            }
-        }
-
-        try {
-            FileOutputStream out = new FileOutputStream(
-                    new File(urlForExcel));
-            workbook.write(out);
-            out.close();
-        }catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    void makeExcel(List<Task3Element> elements, String url);
 }
