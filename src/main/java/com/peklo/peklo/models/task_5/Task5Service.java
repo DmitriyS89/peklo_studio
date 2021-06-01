8363d5796a42cca55275f57287f9dedebf0ccbe6
tariel sapar <tariel.fifa2015@gmail.com>
2021-06-01 16:11:04 +0600

    Change logic in getUserInfo Change params vkApiUrlBuilder

--- src/main/java/com/peklo/peklo/models/task_5/Task5Service.java
+++ src/main/java/com/peklo/peklo/models/task_5/Task5Service.java
@@ -13,11 +13,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.*;
-import java.util.regex.Pattern;
 
 @Service
 public class Task5Service {
@@ -25,62 +22,22 @@ public class Task5Service {
     @Value("${vkApiToken}")
     private String access_token;
 
-    public Document getDocument(String url) throws IOException {
-        try {
-            Connection connect = Jsoup.connect(url);
-            return connect.get();
-        } catch (IOException ioException) {
-            throw new IOException();
-        }
-    }
+    private static final String NULL = "NULL";
 
-    public Vk checkUrl(String url) {
+    public String getUserInfo(String id){
+        String params = String.format("user_ids=%s&fields=contacts", id);
         try {
-            String id = new URI(url).getPath();
-            String substring = id.substring(1);
-            Boolean existVkGroup = existVkGroup(substring);
-            Boolean existVkUser = existVkUser(substring);
-
-            if (existVkGroup && existVkUser)
-                throw new IOException();
-            else if (existVkGroup)
-                return new Group();
-            else if (existVkUser)
-                return new Account();
-        } catch (URISyntaxException | IOException e) {
+            URL url = vkApiUrlBuilder("users.get", params, access_token);
+            return getVkApiAnswer(url);
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return NULL;
     }
 
-    private Boolean existVkUser(String groupId) throws IOException {
-        Pattern pattern = Pattern.compile("first_name");
-        String format = "user_ids=%s&fields=contacts";
-        String params = String.format(format, groupId);
-        if (groupId.startsWith("id")) {
-            params = String.format(format, groupId.substring(2));
-        }
-        URL url = vkApiUrlBuilder("users.get", params, access_token, "5.131");
-        String page = getVkApiAnswer(url);
-        return pattern.matcher(page).find();
-    }
-
-    private Boolean existVkGroup(String groupId) throws IOException {
-        Pattern pattern = Pattern.compile("screen_name");
-        String format = "group_ids=&group_id=%s&fields=";
-        String params = String.format(format, groupId);
-        if (groupId.startsWith("public")) {
-            params = String.format(format, groupId.substring(6));
-        }
-        URL url = vkApiUrlBuilder("groups.getById", params, access_token, "5.131");
-        String page = getVkApiAnswer(url);
-        return pattern.matcher(page).find();
-    }
-
-    private URL vkApiUrlBuilder(String method, String params,
-                                String access_token, String version) throws IOException {
+    private URL vkApiUrlBuilder(String method, String params, String access_token) throws IOException {
         String url = String.format("https://api.vk.com/method/%s?%s&access_token=%s&v=%s",
-                method, params, access_token, version);
+                method, params, access_token, "5.131");
         return new URL(url);
     }
 
