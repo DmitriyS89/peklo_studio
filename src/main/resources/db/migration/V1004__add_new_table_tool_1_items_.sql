e0672904ca410fe77fc2113fbbc4b48eb048a388
tariel sapar <tariel.fifa2015@gmail.com>
2021-06-21 14:48:22 +0600

    Change file migration

--- src/main/resources/db/migration/V1004__add_new_table_tool_1_items_.sql
+++ src/main/resources/db/migration/V1004__add_new_table_tool_1_items_.sql
@@ -1,10 +1,12 @@
 CREATE TABLE `tool_1_items`
 (
-    `id`         bigint NOT NULL AUTO_INCREMENT,
-    `css_path`   varchar(255) DEFAULT NULL,
-    `from_url`   varchar(255) DEFAULT NULL,
-    `html_value` longtext,
-    `time`       varchar(255) DEFAULT NULL,
+    `id`              bigint NOT NULL AUTO_INCREMENT,
+    `css_path`        varchar(255) DEFAULT NULL,
+    `from_url`        varchar(255) DEFAULT NULL,
+    `html_value`      longtext,
+    `time`            varchar(255) DEFAULT NULL,
+    `local_date_time` datetime     DEFAULT NULL,
+    `user_chat_id`    varchar(255) DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = MyISAM
   DEFAULT CHARSET = utf8mb4
