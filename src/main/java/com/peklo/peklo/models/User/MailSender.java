d2941b17aa303fe2a43c578ff597953be37d83e2
nemowaymustdie <nemowaymustdie@gmail.com>
2021-06-24 03:09:01 +0600

    MailSender

--- src/main/java/com/peklo/peklo/models/User/MailSender.java
+++ src/main/java/com/peklo/peklo/models/User/MailSender.java
@@ -3,9 +3,19 @@ package com.peklo.peklo.models.User;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
+
+import javax.mail.Message;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.sql.DataSource;
+import java.io.File;
 
 @RequiredArgsConstructor
 @Service
@@ -24,5 +34,27 @@ public class MailSender {
         mailMessage.setSubject(subject);
         mailMessage.setText(message);
         mailSender.send(mailMessage);
+    }
+
+    public void sendMailWithAttachment(String emailTo, String subject, String text, String fileToAttach) {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        File file = new File(fileToAttach);
+        if (file.exists() && !file.isDirectory()) {
+
+            try {
+                MimeMessageHelper helper = new MimeMessageHelper(message, true);
+                helper.setFrom(username);
+                helper.setTo(emailTo);
+                helper.setSubject(subject);
+                helper.setText(text);
+
+                helper.addAttachment("Something.xlsx", file);
+            } catch (Exception ex) {
+                System.out.println("Error");
+            }
+        }
+
+        mailSender.send(message);
     }
 }
