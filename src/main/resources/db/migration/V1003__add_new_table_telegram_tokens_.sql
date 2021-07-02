edeedeb823128b5693d0add396c18f4df4542b77
tariel sapar <tariel.fifa2015@gmail.com>
2021-07-02 14:45:12 +0600

    final commit: 7
    - Added new migration, deleted needles migration

--- src/main/resources/db/migration/V1003__add_new_table_telegram_tokens_.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `telegramtokens`
-(
-    `user_id` bigint NOT NULL,
-    `token`   varchar(255) DEFAULT NULL,
-    PRIMARY KEY (`user_id`)
-) ENGINE = MyISAM
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci
